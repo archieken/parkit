a56cedd5178a45307a4ab9e4b19183e2ebdbda00
Alex Ramos <alexinbcn19@gmail.com>
2018-03-05 18:50:18 +0100

    add validations to Reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :space
   belongs_to :user
+  validates :start, presence: true
+  validates :end, presence: true
+  validates :total_end, presence: true
 end
