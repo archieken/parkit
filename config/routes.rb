af8935990c553ae43ca73496f4b6d94a478b2d3a
archie kenwright <kenwright.aw@googlemail.com>
2018-03-05 22:11:07 +0100

    add validation to space types

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,44 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'spaces#index' # Root  ‘/’   =>  ‘spaces#index’
+
+  resources :users, only: [:new, :create, :edit, :update]
+
+  # Users : new, create, edit, update
+  # GET    ‘/users/new’   =>   users#new’
+  # POST   ‘/users   =>   users#create’
+  #  GET   ‘/users/:id/edit’   =>    users#edit’
+  # PATCH   ‘/users/:id’   => users#update’
+
+  resources :reservations, only: [:index, :new, :create]
+
+  # Bookings: new, create, index                            (*delete)
+  # GET     ‘/bookings    =>   ‘spaces#index’
+  # GET    ‘/bookings/new’   =>   ‘spaces#new’
+  # POST   ‘/bookings   =>   ‘spaces#create’
+
+  resources :spaces, only: [:index, :new, :create, :edit, :update]
+
+  # Spaces: new, create, index, edit, update
+  # GET     ‘/spaces’    =>   ‘spaces#index’
+  # GET    ‘/spaces/new’   =>   ‘spaces#new’
+  # POST   ‘/spaces’   =>   ‘spaces#create’
+  # GET   ‘/spaces/:id/edit’   =>    ‘spaces#edit’
+  # PATCH   ‘/spaces/:id’   => ‘spaces#update’
 end
+
+
+
+# Profile?????????
+
+
+# Profile: new, create, show, edit, update             (*delete)
+
+# GET    ‘/profiles/new’   =>   profiles#new’
+
+# POST   ‘/profiles   =>   profiles#create’
+
+# GET   ‘/profiles/:id/edit’   =>    profiles#edit’
+
+# PATCH   ‘/profiles/:id’   => ‘profiles#update’
+
