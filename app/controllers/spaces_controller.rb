e0684c690305d848330d13c46541b46237b285ad
archie kenwright <kenwright.aw@googlemail.com>
2018-03-06 13:01:43 +0100

    spaces#index

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,2 +1,16 @@
 class SpacesController < ApplicationController
+
+
+  def index()
+
+  location = params[:location]
+  start = params[:start]
+  end = params[:end]
+
+  @spaces = Space.all.where(:location = location)
+
+  end
+
+
+
 end
