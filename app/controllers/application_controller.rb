5ea46c2e1cb237409a0d44e1048cb542d96efb43
archie kenwright <kenwright.aw@googlemail.com>
2018-03-06 12:08:52 +0100

    changes to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20180305165406) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "fullname", default: "", null: false
+    t.boolean "has_parking", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
