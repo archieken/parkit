ae5b5ad59521ecd746582d793232954d37e59d3b
Alex Ramos <alexinbcn19@gmail.com>
2018-03-06 16:59:22 +0100

    clean up dirty if statement in index action in spaces controller

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -4,29 +4,27 @@ class SpacesController < ApplicationController
   def index
 
 
-  location = params[:location]
-  # start = params[:start]
-  # end = params[:end]
+    location = params[:location]
+    # start = params[:start]
+    # end = params[:end]
 
 
-  if (location.blank?)
-    @spaces = Space.where.not(latitude: nil, longitude: nil)
-    @hash = Gmaps4rails.build_markers(@spaces) do |space, marker|
-    marker.lat space.latitude
-    marker.lng space.longitude
-    marker.infowindow render_to_string(partial: "/spaces/map_box", locals: { space: space })
+    if (location.blank?)
+      @spaces = Space.where.not(latitude: nil, longitude: nil)
+      @hash = Gmaps4rails.build_markers(@spaces) do |space, marker|
+        marker.lat space.latitude
+        marker.lng space.longitude
+        marker.infowindow render_to_string(partial: "/spaces/map_box", locals: { space: space })
+      end
+    else
+      space_not_null = Space.all.where.not(latitude: nil, longitude: nil)
+      @spaces = space_not_null.all.where(address: location)
+      @hash = Gmaps4rails.build_markers(@spaces) do |space, marker|
+        marker.lat space.latitude
+        marker.lng space.longitude
+        marker.infowindow render_to_string(partial: "/spaces/map_box", locals: { space: space })
+      end
     end
   end
-  else
-    space_not_null = Space.all.where.not(latitude: nil, longitude: nil)
-    @spaces = space_not_null.all.where(address: location)
-    @hash = Gmaps4rails.build_markers(@spaces) do |space, marker|
-    marker.lat space.latitude
-    marker.lng space.longitude
-    marker.infowindow render_to_string(partial: "/spaces/map_box", locals: { space: space })
-  end
-
-  end
-
 
 end
