9c7dcb62108c6611665b1207863c7cc30923b039
Alex Ramos <alexinbcn19@gmail.com>
2018-03-06 14:39:15 +0100

    add profile table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306110147) do
+ActiveRecord::Schema.define(version: 20180306133402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.text "description"
+    t.string "avatar"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.date "start"
@@ -62,6 +74,7 @@ ActiveRecord::Schema.define(version: 20180306110147) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
   add_foreign_key "reservations", "spaces"
   add_foreign_key "reservations", "users"
   add_foreign_key "spaces", "users"
