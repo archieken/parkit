8ce59d3ff98610bc44699b13bb01793855b0288a
Joscha Koepke <joscha.koepke@gmail.com>
2018-03-07 11:42:14 +0100

    added new map.js file, restrucured spaces controller with new index method

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -10,21 +10,39 @@ class SpacesController < ApplicationController
      session[:location] = location
    if (location.blank?)
      @spaces = Space.where.not(latitude: nil, longitude: nil)
-     @hash = Gmaps4rails.build_markers(@spaces) do |space, marker|
-       marker.lat space.latitude
-       marker.lng space.longitude
-       marker.infowindow render_to_string(partial: "/spaces/map_box", locals: { space: space })
+
+     @markers = @spaces.map do |space|
+      {
+        lat: space.latitude,
+        lng: space.longitude,
+        infoWindow: { content: render_to_string(partial: "/spaces/map_box", locals: { space: space }) }
+      }
+
+
+     # @hash = Gmaps4rails.build_markers(@spaces) do |space, marker|
+     #   marker.lat space.latitude
+     #   marker.lng space.longitude
+     #   marker.infowindow render_to_string(partial: "/spaces/map_box", locals: { space: space })
      end
    else
      space_not_null = Space.all.where.not(latitude: nil, longitude: nil)
      @spaces = space_not_null.all.where("address ILIKE ?", "%#{location}%")
-     @hash = Gmaps4rails.build_markers(@spaces) do |space, marker|
-       marker.lat space.latitude
-       marker.lng space.longitude
-       marker.infowindow render_to_string(partial: "/spaces/map_box", locals: { space: space })
-      end
+
+     @markers = @spaces.map do |space|
+      {
+        lat: space.latitude,
+        lng: space.longitude,
+        infoWindow: { content: render_to_string(partial: "/spaces/map_box", locals: { space: space }) }
+      }
+
+     # @hash = Gmaps4rails.build_markers(@spaces) do |space, marker|
+     #   marker.lat space.latitude
+     #   marker.lng space.longitude
+     #   marker.infowindow render_to_string(partial: "/spaces/map_box", locals: { space: space })
+     #  end
     end
   end
+end
 
  def new
    @space = Space.new
---
+++ app/javascript/packs/map.js
@@ -0,0 +1,194 @@
+import GMaps from 'gmaps/gmaps.js';
+
+const mapElement = document.getElementById('map');
+if (mapElement) { // don't try to build a map if there's no div#map to inject in
+  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  window.mapx = map
+  const markers = JSON.parse(mapElement.dataset.markers);
+  map.addMarkers(markers);
+  if (markers.length === 0) {
+    map.setZoom(2);
+  } else if (markers.length === 1) {
+    map.setCenter(markers[0].lat, markers[0].lng);
+    map.setZoom(14);
+  } else {
+    map.fitLatLngBounds(markers);
+  }
+}
+
+const styles = [ {
+        "featureType": "all",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "weight": "2.00"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#9c9c9c"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#eeeeee"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#7b7b7b"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#46bcec"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#c8d7d4"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#070707"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    } ];
+
+mapx.addStyle({
+  styles: styles,
+  mapTypeId: 'map_style'
+});
+mapx.setStyle('map_style');
+
+
