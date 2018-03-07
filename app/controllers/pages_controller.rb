e61f01347898b1b61d4d1f032b92189e36092981
Alex Ramos <alexinbcn19@gmail.com>
2018-03-07 16:13:17 +0100

    add how it works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :about]
   def home
+  end
+
+  def about
   end
 end
