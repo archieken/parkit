93885e5abc64fe52b4cd0813386625edb3fc972e
Joscha Koepke <joscha.koepke@gmail.com>
2018-03-07 20:47:50 +0100

    added auto complete

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,10 +1,13 @@
 import GMaps from 'gmaps/gmaps.js';
 
+import { autocomplete } from '../components/autocomplete';
+
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
   window.mapx = map
   const markers = JSON.parse(mapElement.dataset.markers);
+
 
   map.addMarkers(markers);
   if (markers.length === 0) {
@@ -16,6 +19,8 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
     map.fitLatLngBounds(markers);
   }
 }
+
+autocomplete();
 
 const styles = [ {
         "featureType": "all",
@@ -191,6 +196,7 @@ mapx.addStyle({
   mapTypeId: 'map_style'
 });
 mapx.setStyle('map_style');
+
 
 
 
