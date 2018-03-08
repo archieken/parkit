f644d9919a7d6a9ea65d6247f11db15a92d6b1e0
archie kenwright <kenwright.aw@googlemail.com>
2018-03-08 18:44:07 +0100

    1 dec

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -5,6 +5,10 @@ output.innerHTML = slider.value + " KM";
  if (slider.value < 1000) {
     output.innerHTML = slider.value + " Meters";
   }
+
+  else if (slider.value == 1000 || slider.value == 2000 ) {
+    output.innerHTML = (slider.value/1000).toFixed(0) + " KM";
+  }
   else {
     output.innerHTML = (slider.value/1000).toFixed(2) + " KM";
   }
@@ -13,6 +17,11 @@ slider.oninput = function() {
   if (this.value < 1000) {
     output.innerHTML = this.value  + " Meters";
   }
+
+  else if (slider.value == 1000 || slider.value == 2000 ) {
+    output.innerHTML = (slider.value/1000).toFixed(0) + " KM";
+  }
+
   else {
     output.innerHTML = (this.value/1000).toFixed(2) + " KM";
   }
